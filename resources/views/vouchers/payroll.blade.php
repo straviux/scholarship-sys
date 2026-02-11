1e084bdc88b8ea57b26e8000cef4a5b17477df56
arc.straviux <arvin.heredero@gmail.com>
2026-02-11 10:32:40 +0800

    fix(payroll): update payroll title to include 'SEM' in academic year display

--- resources/views/vouchers/payroll.blade.php
+++ resources/views/vouchers/payroll.blade.php
@@ -112,7 +112,7 @@
             <!-- Payroll Title Row -->
             <div class="row border-none">
                 <div class="col col-grow col-left col-vcenter">
-                    <span class="font-bold" style="font-size:10pt">FOR ACADEMIC YEAR {{ explode(' ', $term)[0] ?? $term }} {{ $academicYear }} </span>
+                    <span class="font-bold" style="font-size:10pt">FOR ACADEMIC YEAR {{ explode(' ', $term)[0] ?? $term }} SEM {{ $academicYear }} </span>
                 </div>
             </div>
 
