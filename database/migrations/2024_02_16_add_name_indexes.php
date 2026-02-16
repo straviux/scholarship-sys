d142c7732122a036153c6145133eced9487c2585
arc.straviux <arvin.heredero@gmail.com>
2026-02-16 10:11:15 +0800

    fix(migration): streamline index creation for name validation in scholarship_profiles table

--- database/migrations/2024_02_16_add_name_indexes.php
+++ database/migrations/2024_02_16_add_name_indexes.php
@@ -3,7 +3,6 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -20,16 +19,9 @@ return new class extends Migration
         }
 
         Schema::table('scholarship_profiles', function (Blueprint $table) {
-            // Check if index already exists before adding
-            $indexName = 'idx_name_validation';
-            $sm = Schema::getConnection()->getDoctrineSchemaManager();
-            $indexes = $sm->listTableIndexes('scholarship_profiles');
-
-            if (!isset($indexes[$indexName])) {
-                // Create composite index for first_name and last_name
-                // This will speed up the duplicate name validation query significantly
-                $table->index(['first_name', 'last_name'], $indexName);
-            }
+            // Create composite index for first_name and last_name
+            // This will speed up the duplicate name validation query significantly
+            $table->index(['first_name', 'last_name'], 'idx_name_validation');
         });
     }
 
